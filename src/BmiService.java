28406bb74f599d52331bc8fc9c76f3fc1c461038
pricheska <den-ryab2019@yandex.ru>
2022-12-22 17:28:29 +0300

    Solving the wrong formula

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int tall, int mass) {
-        int indexBodyWeight = mass * tall * tall;
+    public double calculate(double tall, int mass) {
+        double indexBodyWeight = mass / (tall * tall);
         return indexBodyWeight;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int mass = 58;
-        int tall = 183;
-        int index = service.calculate(tall, mass);
-    System.out.println(index);
+        double tall = 1.83;
+        double index = service.calculate(tall, mass);
+        
+        System.out.println(Math.round(index));
     }
 }
